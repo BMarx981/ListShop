5489563fc946e4ed3ec4c87a0c01fdcc54ba8a24
Brian Marx <b.marx981@gmail.com>
2017-11-24 16:04:57 -0500

    Initial Commit

---
+++ ListShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListShop
+//
+//  Created by Marx, Brian on 11/24/17.
+//  Copyright © 2017 Marx, Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
