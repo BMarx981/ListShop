9c720431a75f27eb82b013fb99836bb3c815b7c3
Brian Marx <b.marx981@gmail.com>
2017-11-29 15:22:19 -0500

    Adjust constraints

--- ListShop/Controller/ListTableViewController.swift
+++ ListShop/Controller/ListTableViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    var fruitsAndVegtables = [
+        "cucumbers", "carrots", "apples", "spinach"
+    ]
+    
+    var carbs = [
+        "cereal", "bread", "pasta",
+    ]
+    
+    var lists = [[String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +27,7 @@ class ListTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         self.navigationItem.rightBarButtonItem = self.editButtonItem
+        lists = [fruitsAndVegtables, carbs]
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,34 +39,64 @@ class ListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return lists.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if section == 0 {
+            return fruitsAndVegtables.count
+        } else if section == 1 {
+            return carbs.count
+        }
         return 0
     }
 
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let uiView = UILabel()
-        
-        if section == 0 {
-            uiView.text =  "First Section"
-        } else if section == 1 {
-            uiView.text =  "Second Section"
-        }
-        return uiView
+        let button = UIButton(type: .system)
+        button.setTitle("Close", for: .normal)
+        button.addTarget(self, action: #selector(handleOpenClose), for: .touchUpInside)
+        return button
     }
-    /*
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 34
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "id", for: indexPath)
 
-        // Configure the cell...
-
+        if indexPath.section == 0 {
+            cell.textLabel?.text = fruitsAndVegtables[indexPath.row]
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = carbs[indexPath.row]
+        }
         return cell
     }
-    */
-
+    
+    @objc func handleOpenClose() {
+        listSubviewsOfView(view: tableView)
+    }
+    
+    func listSubviewsOfView(view:UIView){
+        print("@@@@@@@@@@@@@@**********************@@@@@@@@@@@@@@@@@@@@")
+        // Get the subviews of the view
+        var subviews = view.subviews
+        
+        // Return if there are no subviews
+        if subviews.count == 0 {
+            return
+        }
+        
+        for subview : AnyObject in subviews{
+            
+            // Do what you want to do with the subview
+            print(subview)
+            
+            // List the subviews of subview
+            listSubviewsOfView(view: subview as! UIView)
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
